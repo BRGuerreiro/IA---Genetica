7412a21ee4c189613149b9b582e5bd278b128c56
BRGuerreiro <32147568+BRGuerreiro@users.noreply.github.com>
2018-03-28 21:45:52 -0300

    Subindo Começo GA
    Subindo Começo GA

---
+++ GACaixeiroViajante/GACaixeiroViajante/Cidade.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GACaixeiroViajante
+{
+    class Cidade
+    {
+        private int x;
+        //Posição Y da cidade no mapa
+        private int y;
+        //Nome da cidade
+        private String name;
+
+        //Construtor da classe
+        public Cidade(String name1, int x1, int y1)
+        {
+            name = name1;
+            x = x1;
+            y = y1;
+        }
+
+        
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+        public String getName()
+        {
+            return name;
+        }
+
+        public double distanciaParaProximaCidade(Cidade cidade)
+        {
+            double distancia = 0;
+            return distancia;
+        }
+
+    }
+}
